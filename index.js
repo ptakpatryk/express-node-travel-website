04ccae12a2bd72f4da466402d753c2b42e0fbc30
Patryk Ptak <ptaszqq@gmail.com>
2021-05-06 21:22:27 +0100

    handlers tests

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const handlers = require('./lib/handlers')
+
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortuneCookie = require('./lib/fortune');
 
 const app = express()
-
-console.log(JSON.stringify(fortuneCookie))
 
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
@@ -15,26 +14,15 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortuneCookie.getCookie()})
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 // Custom 500 page
 
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}`))
