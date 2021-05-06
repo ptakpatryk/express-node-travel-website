f2f7767ef5359bf93bba969c4dde45d251ea98fe
Patryk Ptak <ptaszqq@gmail.com>
2021-05-06 20:39:58 +0100

    implement handlebars templating

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const fortuneCookie = require('./lib/fortune');
 
 const app = express()
 
+console.log(JSON.stringify(fortuneCookie))
+
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
 const port = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-  res.type('text/plain')
-  res.status(200)
-  res.send('Homepage')
+  res.render('home')
 })
 
 app.get('/about', (req, res) => {
-  res.type('text/plain')
-  res.status(200)
-  res.send('About Page')
+  res.render('about', { fortune: fortuneCookie.getCookie()})
 })
 
 // custom 404 page
 app.use((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 // Custom 500 page
 
 app.use((err, req, res, next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}`))
